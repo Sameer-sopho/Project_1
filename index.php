26de227b0b488313a3917708819e05d830337c62
Sameer Jathavedan <sameerj137@gmail.com>
2017-03-21 12:13:41 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title> Input Page </title>
+    </head>
+    <body style="text-align: center;">
+        <h1> Input URL </h1>
+        <br><br><br>
+        <form>
+            Enter your URL here:  <input type="text" name="URL" placeholder="URL">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
