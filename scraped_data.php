838f2ab243d228d0b7f9aa3585ae84174c8e52b0
Sameer Jathavedan <sameerj137@gmail.com>
2017-04-05 12:47:38 +0000

    Scraped and stored facilites of each college in array

--- scraped_data.php
+++ scraped_data.php
@@ -17,11 +17,64 @@ $college = array();
 
 // find and store the names
 preg_match_all('/<h2 class="tuple-clg-heading"><a href="(?:.*?)">(.*?)<\/a>/', $ret, $matcha);
-$college["cname"] = $matcha[1];
+$college["name"] = $matcha[1];
 
-//find and store the addresses
+// find and store the addresses
 preg_match_all('/<p>\| (.*?)<\/p><\/h2>/', $ret, $matchb);
-$college["cadd"] = $matchb[1];
+$college["address"] = $matchb[1];
+
+// find the facilities code block of all colleges
+preg_match_all('/<ul class="facility-icons">(.*?)<\/ul>/s', $ret, $matchc);
+
+// separate and store the facilities of each individual college 
+for( $i=0 ; $i < count($matchc[1]) ; $i++ )
+{
+    preg_match_all('/<h3>(.*?)<\/h3>/', $matchc[1][$i], $matchca);
+    $college["facilities"][$i] = implode(', ', $matchca[1]);
+    
+}
+
+/*
+
+// find the reviews code block of all colleges
+preg_match_all('/<div class="tuple-exam-dtls">(.*?)<p class="clr">/s', $ret, $matchd);
+
+
+// store the reviews of each individual college
+for( $i=0 ; $i < count($matchd[1]) ; $i++ )
+{
+    if(preg_match_all('/<span><b>(\d+)<\/b>/', $matchd[1][$i], $matchda))
+    {
+        $college["reviews"][$i] = $matchda[1][0];
+    }
+    else
+    {
+        $college["reviews"][$i] = '0';
+    }
+    
+}
+
+*/
+
+/*
+
+if(preg_match_all('/<div class="tuple-exam-dtls">/', $ret, $matchc))
+{
+    $temp = "success";
+}
+else
+{
+    $temp = "failure";
+
+}
+
+*/
+
+
+
+
+
+
 
 ?>
 
@@ -38,9 +91,17 @@ $college["cadd"] = $matchb[1];
         print_r("<pre>");
         //print_r($matcha[1]);
         //print_r($matchb[1]);
-        print_r($college["cname"]);
-        print_r($college["cadd"]);
-        
+        //print_r($college["name"]);
+        //print_r($college["address"]);
+        //print_r($college["facilities"]);
+        //print_r($college["reviews"]);
+        //print($matchda);
+        //print_r($matchc[1]);
+        //print_r($college);
+        //print($temp);
+        //echo "<br><br>";
+        //print_r($matchc[1]);
+        //print_r($matchd[1]);
         
         ?>
     </body>
